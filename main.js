21aa5ecb98548ec19a8fc9974e61992747115e9a
Marco Túlio Sá de Paiva <tulio.paiva10@gmail.com>
2022-09-05 18:11:05 -0300

    Trabalhando com JS, construindo carrinho de compras INCOMPLETO

--- main.js
+++ main.js
@@ -1,15 +1,28 @@
-var planos = document.querySelectorAll('#plano');
-var carrinho = 0;
-
+var planoInternet = document.querySelectorAll('#plano__internet');
+var planoFixo = document.querySelectorAll('#plano__fixo');
+var cart = 0;
 /*Evento de clique para selecionar os inputs dos planos*/
 
-planos.forEach(plano => {
+planoInternet.forEach(plano => {
     plano.addEventListener('click', event => {
         const selecionado = event.target;
-        console.log(selecionado.value);
+        console.log(selecionado);
+        
+        cart = (parseFloat(cart) + parseFloat(selecionado.value)).toFixed(2);
+        document.querySelector('#carrinho__valor').innerHTML = 'R$' + cart;
+        console.log(cart);
     }
     )
 })
 
 
-
+planoFixo.forEach(plano => {
+    plano.addEventListener('click', event => {
+        const selecionado = event.target;
+        
+        cart = (parseFloat(cart) + parseFloat(selecionado.value)).toFixed(2);
+        document.querySelector('#carrinho__valor').innerHTML = 'R$' + cart;
+        console.log(cart);
+    }
+    )
+})
