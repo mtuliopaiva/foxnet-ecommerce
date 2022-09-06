78f97fc7ad78a4d86e892b5ef8e44482b175bf3e
Marco Túlio Sá de Paiva <tulio.paiva10@gmail.com>
2022-09-06 12:48:21 -0300

    Trabalhando com JS, trabalhando com data attributes

--- main.js
+++ main.js
@@ -1,28 +1,37 @@
-var planoInternet = document.querySelectorAll('#plano__internet');
-var planoFixo = document.querySelectorAll('#plano__fixo');
+// var planoInternet = document.querySelectorAll('#plano__internet');
+
+
+var planos = document.querySelectorAll('.plano');
+
+
+
+
 var cart = 0;
 /*Evento de clique para selecionar os inputs dos planos*/
+planos.forEach(plano => {
+    plano.addEventListener('click', evento => {
 
-planoInternet.forEach(plano => {
-    plano.addEventListener('click', event => {
-        const selecionado = event.target;
-        console.log(selecionado);
-        
-        cart = (parseFloat(cart) + parseFloat(selecionado.value)).toFixed(2);
-        document.querySelector('#carrinho__valor').innerHTML = 'R$' + cart;
-        console.log(cart);
+        console.log(evento.target);
+        const selecionadoValor = evento.target.dataset.valor
+        const selecionadoNome = evento.target.dataset.nome
+
+        console.log(selecionadoValor);
+        console.log(selecionadoNome);
     }
     )
 })
 
 
-planoFixo.forEach(plano => {
-    plano.addEventListener('click', event => {
-        const selecionado = event.target;
+
+// planoInternet.forEach(plano => {
+//     plano.addEventListener('click', event => {
+//         const selecionadoValor = event.target.dataset.valor;
+//         console.log(selecionadoValor);
+
+
         
-        cart = (parseFloat(cart) + parseFloat(selecionado.value)).toFixed(2);
-        document.querySelector('#carrinho__valor').innerHTML = 'R$' + cart;
-        console.log(cart);
-    }
-    )
-})
+//         cart = (parseFloat(cart) + parseFloat(selecionadoValor.dataset.valor)).toFixed(2);
+//         document.querySelector('#carrinho__valor').innerHTML = 'R$' + cart;
+//     }
+//     )
+// })
