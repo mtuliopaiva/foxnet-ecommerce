817bf50c5fbcb9d70a9e240edcf0b9e61078a434
Marco Túlio Sá de Paiva <tulio.paiva10@gmail.com>
2022-09-08 23:42:49 -0300

    Trabalhando com CSS,proximo passo trabalhando com o carrinho CSS e JS

--- main.js
+++ main.js
@@ -11,12 +11,83 @@ var cart = 0;
 
 
 
-detalhes.forEach(detalhes => {
-    detalhes.addEventListener('click', evento =>{
-        console.log(evento.target);
+// detalhes.forEach(detalhes => {
+//     detalhes.addEventListener('click', evento =>{
+        
+//         console.log(evento.target);
+//         console.log(evento);
+//         const texto = evento.path[1].children[0];
+//         const valor = evento.path[1].children[1];
+//         const detalhe = evento.path[1].children[2];
+//         console.log(evento.path[1]);
+//         console.log(texto);
+//         console.log(valor);
+//         // texto.style.cssText = 'display: none;';
+//         // valor.style.cssText = 'display: none;';
+//         // detalhe.textContent = "- detalhes";
 
-    })
-})
+//         switch(evento.target.dataset.nome) {
+//             case '300 MB':
+//                 let btn__detalhe ='1';
+//                 console.log("Primeiro caso");
+//                 if(btn__detalhe =='1'){
+                    
+//                     texto.style.cssText = 'opacity: 0';
+//                     valor.style.cssText = 'font-size: 0.8rem';
+//                     valor.textContent = "Bonus: + 300 MB";
+//                     detalhe.textContent = "- detalhes";
+//                     btn__detalhe=0;
+//                 }
+//                 if(btn__detalhe =='0'){
+//                     texto.style.cssText = 'opacity: 1';
+//                     valor.style.cssText = 'font-size: 1rem';
+//                     valor.textContent = "R$ 99,90";
+//                     detalhe.textContent = "+ detalhes";
+//                     btn__detalhe=0;
+//                 }
+
+//                 break;
+
+//             case '200 MB':
+//                 console.log("Segundo caso");
+//                 detalhe.textContent = "- detalhes";
+//                 break;
+
+//             case '100 MB':
+//                 console.log("Terceiro caso");
+//                 detalhe.textContent = "- detalhes";
+//                 break;
+
+//             case 'ilimitado':
+//                 console.log("Quarto caso");
+//                 detalhe.textContent = "- detalhes";
+//                 break;
+
+//             case 'Ultimate HD':
+//                 console.log("5 caso");
+//                 detalhe.textContent = "- detalhes";
+//                 break;
+
+//             case 'Full HD':
+//                 console.log("6 caso");
+//                 detalhe.textContent = "- detalhes";
+//                 break;
+//         }
+
+
+        // if(evento.target.dataset.nome=='300 MB'){
+        //     console.log("Pirmeiro plano");
+        // }
+        // else{
+        //     if(evento.target.dataset.nome=='200 MB'){
+        //         console.log("Segundo plano");
+        //     }
+        //     else{
+        //         console.log("Terceiro plano");
+        //     }
+        // }
+//     })
+// })
 /*Evento de clique para selecionar os inputs dos planos*/
 internet.forEach(plano => {
     plano.addEventListener('click', evento => {
